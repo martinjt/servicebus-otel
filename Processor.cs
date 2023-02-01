643268edd05d6ba947bba25d6297a31eb3ba4352
Martin Thwaites <github@my2cents.co.uk>
2023-02-01 16:32:27 +0000

    added tracing

--- Processor.cs
+++ Processor.cs
@@ -1,4 +1,7 @@
+using System.Diagnostics;
 using Azure.Messaging.ServiceBus;
+using OpenTelemetry;
+using OpenTelemetry.Context.Propagation;
 
 public class MessageProcessor
 {
@@ -9,9 +12,8 @@ public class MessageProcessor
     {
         _client = client;
         _processor = client.CreateProcessor("main", new ServiceBusProcessorOptions());
-        _processor.ProcessMessageAsync += MessageHandler;
+        _processor.ProcessMessageAsync += TracedMessageHandler;
         _processor.ProcessErrorAsync += ErrorHandler;
-
     }
 
     public async Task StartReceiving()
@@ -19,9 +21,27 @@ public class MessageProcessor
         await _processor.StartProcessingAsync();
     }
 
+    async Task TracedMessageHandler(ProcessMessageEventArgs args)
+    {
+        var context = Propagators.DefaultTextMapPropagator.Extract(new PropagationContext(
+            new ActivityContext(),
+            Baggage.Current
+        ), 
+            args.Message.ApplicationProperties, 
+            ExtractContextFromApplicationProperties);
+
+        using var activity = ActivityConfig.Source
+            .StartActivity("Receive Message", 
+                ActivityKind.Consumer, null, 
+                links: new List<ActivityLink> {
+                    new ActivityLink(context.ActivityContext)
+            });
+        
+        await MessageHandler(args);
+    }
+
     async Task MessageHandler(ProcessMessageEventArgs args)
     {
-        using var activity = ActivityConfig.Source.StartActivity("Receive Message");
         string body = args.Message.Body.ToString();
         Console.WriteLine($"Received: {body}");
 
@@ -35,4 +55,20 @@ public class MessageProcessor
         Console.WriteLine(args.Exception.ToString());
         return Task.CompletedTask;
     }
+
+    /// <summary>
+    /// Extract values from the 
+    /// </summary>
+    /// <param name="properties"></param>
+    /// <param name="key"></param>
+    /// <returns></returns>
+    internal static IEnumerable<string> ExtractContextFromApplicationProperties(IReadOnlyDictionary<string, object> properties, string key)
+    {
+        var valueFromProps = properties.TryGetValue(key, out var propertyValue)
+                ? propertyValue?.ToString() ?? ""
+                : "";
+
+        return new List<string> { valueFromProps };
+    }
+
 }
--- Sender.cs
+++ Sender.cs
@@ -1,4 +1,7 @@
+using System.Diagnostics;
 using Azure.Messaging.ServiceBus;
+using OpenTelemetry;
+using OpenTelemetry.Context.Propagation;
 
 public class MessageSender
 {
@@ -15,8 +18,15 @@ public class MessageSender
 
         for (int i = 1; i <= numOfMessages; i++)
         {
+            var message = new ServiceBusMessage($"Message {i}");
+            Propagators.DefaultTextMapPropagator.Inject(
+                new PropagationContext(Activity.Current?.Context ?? default, Baggage.Current),
+                message,
+                InjectPropertiesToServiceBusMessage
+            );
+
             // try adding a message to the batch
-            if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+            if (!messageBatch.TryAddMessage(message))
             {
                 // if it is too large for the batch
                 throw new Exception($"The message {i} is too large to fit in the batch.");
@@ -25,4 +35,14 @@ public class MessageSender
         await _sender.SendMessagesAsync(messageBatch);
         Console.WriteLine($"A batch of {numOfMessages} messages has been published to the queue.");
     }
+
+    /// <summary>
+    /// Inject values into the ServiceBus Message ApplicationProperties
+    /// </summary>
+    /// <param name="message">The message to inject into</param>
+    /// <param name="key">The key to set</param>
+    /// <param name="value">The value to set in the provided key</param>
+    internal static void InjectPropertiesToServiceBusMessage(ServiceBusMessage message, string key, string value)
+        => message.ApplicationProperties[key] = value;
+
 }
